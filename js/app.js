8a320ae9f1a01d9698945862cf95d170b3c7d4cf
roberto <rbustios92@gmail.com>
2024-08-26 20:18:30 -0500

    preentrega 3, manejo de DOM, eventos y JSON

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // Simuladora de edad y peso terrestre en el resto de planetas del sistema solar
 // Desarrollado por Roberto Bustíos
-// v0.2 alpha
+// v0.3 alpha
 
-/*- Declaración e inicialización de variables y constantes -*/
+/*- DECLARACIÓN DE CONSTANTES Y VARIABLES -*/
 
 const planetas = [
     { nombre: "Mercurio", orbita: "0.24", gravedad: "3.7" },
@@ -15,14 +15,23 @@ const planetas = [
     { nombre: "Plutón", orbita: "247", gravedad: "0.62" },
 ]
 
-const usuario = {
-    nombre: prompt("¡Bienvenido! ¿Cuál es tu nombre?"),
-    edad: parseInt(prompt("Ingresa tu edad")),
-    peso: parseFloat(prompt("Ingresa tu peso aproximado")),
-    planeta: prompt("Escribe el planeta:\n-> Mercurio\n-> Venus\n-> Marte\n-> Júpiter\n-> Saturno\n-> Urano\n-> Neptuno\n-> Plutón")
-};
+const paginas = document.querySelectorAll('.page');
+let paginaActual = 0;
 
-/*- Funciones -*/
+const botonIzquierda = document.querySelector('.page-left');
+const botonDerecha = document.querySelector('.page-right');
+const calcular = document.getElementById('calcular');
+const calcQuiz = document.getElementById('calc-quiz');
+
+/*- FUNCIONES -*/
+
+function mostrarPagina(indice) {
+    paginas.forEach((paginas, i) => {
+        paginas.classList.toggle('active', i === indice);
+    });
+    botonIzquierda.disabled = (indice === 0);
+    botonDerecha.disabled = (indice === paginas.length - 1);
+}
 
 function calcularEdad(edad, orbita) {
     return (edad / orbita).toFixed(2);
@@ -32,46 +41,182 @@ function calcularPeso(peso, gravedad) {
     return ((peso * gravedad) / 9.81).toFixed(2);
 }
 
-function main(usuario) {
+function main() {
+    const usuario = {
+        nombre: document.getElementById("name").value.trim(),
+        edad: parseInt(document.getElementById("age").value),
+        peso: parseFloat(document.getElementById("weight").value),
+        planeta: document.querySelector('input[name="planet"]:checked')?.value
+    };
 
-    const planetaElegido = planetas.find(p => p.nombre.toLowerCase() === usuario.planeta.toLowerCase());
+    const mensajeError = document.getElementById("errormessage");
+    const mensajeExito = document.getElementById("successmessage");
+    mensajeError.textContent = "";
+    mensajeExito.textContent = "";
 
-    /*- Validaciones -*/
-
+    if (usuario.nombre === "" || usuario.edad === "" || usuario.peso === "" || usuario.planeta === undefined) {
+        mensajeError.textContent = "¡Faltan llenar campos!";
+        return;
+    }
     if (!usuario.nombre || !isNaN(usuario.nombre)) {
-        console.error("¡Nombre inválido!");
+        mensajeError.textContent = "¡Nombre inválido!";
         return;
     }
     if (!usuario.edad || isNaN(usuario.edad)) {
-        console.error("¡La edad ingresada no es válida!");
+        mensajeError.textContent = "¡La edad ingresada no es válida!";
         return;
     }
     if (!usuario.peso || isNaN(usuario.peso)) {
-        console.error("¡El peso ingresado no es válido!");
-        return;
-    }
-    if (!planetaElegido) {
-        console.error("¡El planeta no es uno de la lista!");
+        mensajeError.textContent = "¡El peso ingresado no es válido!";
         return;
     }
 
-    // Inicializa las variables que contendrán los valores de las funciones calcularEdad y calcularPeso
+    const planetaElegido = planetas.find(p => p.nombre.toLowerCase() === usuario.planeta.toLowerCase());
+
+    if (!planetaElegido) {
+        mensajeError.textContent = "¡El planeta no es uno de la lista!";
+        return;
+    }
 
     const edadPlaneta = calcularEdad(usuario.edad, planetaElegido.orbita);
     const pesoPlaneta = calcularPeso(usuario.peso, planetaElegido.gravedad);
 
-    /*- Si todo sale bien, muestra lo siguiente: -*/
+    mensajeExito.innerHTML = `
+    <h3>${usuario.nombre}, en la Tierra:</h3>
+    <p>Tu edad es <strong>${usuario.edad} años</strong> y pesas <strong>${usuario.peso} kilos</strong></p><br>
+    <h3>En ${usuario.planeta}:</h3>
+    <div class="message-container">
+        <div class="message message-age">
+            <i class="fa-solid fa-calendar-days"></i><br>
+            <p>Tu edad sería:<br><strong>${edadPlaneta} años</strong></p>
+        </div>
+        <div class="message message-weight">
+            <i class="fa-solid fa-weight-scale"></i><br>
+            <p>Tu peso sería:<br><strong>${pesoPlaneta} kilos</strong></p>
+        </div>
+    </div>
+    `;
 
-    console.log(usuario.nombre + ",");
-    console.log("--------------");
-    console.log("Tu edad es: " + usuario.edad + " en años terrestres");
-    console.log("Si vivieras en " + usuario.planeta + ", tu edad sería: " + edadPlaneta + " años");
-    console.log("--------------");
-    console.log("Tu peso es: " + usuario.peso + " en la Tierra");
-    console.log("Si vivieras en " + usuario.planeta + ", tu peso sería: " + pesoPlaneta + " kilos");
-    console.log("--------------");
+    const botonRegresar = document.createElement('button');
+    botonRegresar.textContent = 'Regresar';
+    botonRegresar.addEventListener('click', () => {
+        mensajeExito.style.display = 'none';
+        calcQuiz.style.display = 'block';
+        paginaActual = 0; // Reiniciar a la primera página
+        mostrarPagina(paginaActual);
+        focusNextInput();
+    });
+    mensajeExito.appendChild(botonRegresar);
+
+    const resultadoNuevo = {
+        id: Date.now(), // Se usa un timestamp como identificador único
+        nombre: usuario.nombre,
+        planeta: usuario.planeta,
+        edad: usuario.edad,
+        edadplaneta: edadPlaneta,
+        peso: usuario.peso,
+        pesoplaneta: pesoPlaneta
+    };
+
+    let resultados = JSON.parse(localStorage.getItem("resultados")) || [];
+    resultados.push(resultadoNuevo);
+    localStorage.setItem("resultados", JSON.stringify(resultados));
+
+    calcQuiz.style.display = "none";
+    mensajeExito.style.display = "flex";
+
+    mostrarResultados();
+
+    usuario.nombre.innerHTML == "";
+    usuario.edad.textContent = "";
 }
 
-/*- Ejecución principal de la función main () -*/
+function mostrarResultados() {
+    let resultados = JSON.parse(localStorage.getItem("resultados")) || [];
+    const tbody = document.querySelector('.table-results tbody');
 
-main(usuario);
+    tbody.innerHTML = resultados.length ? '' : `<tr id="res-no-results">
+                <td colspan="8">No hay resultados calculados</td>
+            </tr>`;
+
+    resultados.forEach(resultado => {
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = `
+            <td>${resultado.id}</td>
+            <td>${resultado.nombre}</td>
+            <td>${resultado.planeta}</td>
+            <td>${resultado.edad} años</td>
+            <td>${resultado.edadplaneta} años</td>
+            <td>${resultado.peso} kilos</td>
+            <td>${resultado.pesoplaneta} kilos</td>
+            <td><button class="btn btn-light rounded-0 borrar-resultado" data-id="${resultado.id}">&#10006;</button></td>
+        `;
+
+        tbody.appendChild(tr);
+    });
+
+    document.querySelectorAll('.borrar-resultado').forEach(button => {
+        button.addEventListener('click', (event) => {
+            const id = event.target.getAttribute('data-id');
+            resultados = resultados.filter(res => res.id != id);
+            localStorage.setItem('resultados', JSON.stringify(resultados));
+            mostrarResultados();
+        });
+    });
+}
+
+function focusNextInput() {
+    const currentInput = paginas[paginaActual].querySelector('input');
+    if (currentInput) {
+        currentInput.focus();
+    }
+}
+
+/*- INSTRUCCIONES -*/
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    mostrarResultados();
+});
+
+botonDerecha.addEventListener('click', () => {
+    if (paginaActual < paginas.length - 1) {
+        paginaActual++;
+        mostrarPagina(paginaActual);
+        focusNextInput();
+    }
+});
+
+botonIzquierda.addEventListener('click', () => {
+    if (paginaActual > 0) {
+        paginaActual--;
+        mostrarPagina(paginaActual);
+        focusNextInput();
+    }
+});
+
+mostrarPagina(paginaActual);
+
+paginas.forEach((pagina, index) => {
+    const inputs = pagina.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                if (index < paginas.length - 1) {
+                    paginaActual++;
+                    mostrarPagina(paginaActual);
+                    focusNextInput();
+                } else if (index === paginas.length - 1) {
+                    main();
+                }
+            }
+        });
+    });
+});
+
+calcular.addEventListener('click', main);
+
+document.getElementById("borrar-ls").addEventListener('click', () => {
+    localStorage.clear();
+    location.reload();
+});
